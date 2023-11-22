9e0bb7ddc60a3fc4971cca92b9d764dbe60a3894
shihao.liu <huangfeng.lsh@raycloud.com>
2023-11-22 09:44:20 +0800

    first commit

---
+++ src/main/java/com/liushihao/learnhub/LshLearnHubApplication.java
@@ -0,0 +1,16 @@
+package com.liushihao.learnhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author admin
+ */
+@SpringBootApplication
+public class LshLearnHubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LshLearnHubApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/liushihao/learnhub/LshLearnHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.liushihao.learnhub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LshLearnHubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
